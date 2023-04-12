75d2846cb45992a32747dadc325fbfdea85f8e01
Audun <audunwo@uio.no>
2023-04-12 14:32:55 +0200

    more reorganisation

--- _examples/_img/main_test.go
+++ _examples/_img/main_test.go
@@ -1,9 +1,10 @@
 package main
 
 import (
-	"testing"
 	"flag"
+	"testing"
 )
+
 // We pass a flag to indicate which method to benchmark
 // This is done because benchcmp will compare the same benchmark method - here BenchmarkEither()
 var flagvar = flag.Int("mode", 0, "mode 0 is origin, mode 1 is target")
@@ -12,14 +13,14 @@ var src = loadImage("testdata/test_rail.jpg")
 // Runs a benchmark on the original method in main
 func BenchmarkOrigin(b *testing.B) {
 	for i := 0; i < b.N; i++ {
-		runOrigin(src);
+		runOrigin(src)
 	}
 }
 
 // Runs a benchmark on the new method in main
 func BenchmarkTarget(b *testing.B) {
 	for i := 0; i < b.N; i++ {
-		runTarget(src);
+		runTarget(src)
 	}
 }
 
@@ -29,11 +30,11 @@ func BenchmarkEither(b *testing.B) {
 	switch *flagvar {
 	case 0:
 		for i := 0; i < b.N; i++ {
-			runOrigin(src);
+			runOrigin(src)
 		}
 	case 1:
 		for i := 0; i < b.N; i++ {
-			runTarget(src);
+			runTarget(src)
 		}
-	}	
+	}
 }
